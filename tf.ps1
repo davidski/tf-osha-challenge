7a48618e7618b7f10f0ca615e72ec66af99d5832
David F. Severski <davidski@deadheaven.com>
2017-04-28 12:58:58 -0700

    Deprecate PoSH driven TF.

--- tf.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param(
-  [Parameter(Mandatory=$False, position=1)]
-  [string]$command = "plan"
-)
-
-$ip = "$(invoke-restmethod https://api.ipify.org)"
-terraform $command -var ''home_ip=""$ip""''
