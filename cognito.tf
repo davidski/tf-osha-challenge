0bed216db9ca6fd8bf34fc1bfb1000a405677f14
David F. Severski <davidski@deadheaven.com>
2017-04-28 20:34:49 -0700

    Specify regional provider

--- cognito.tf
+++ cognito.tf
@@ -5,6 +5,7 @@
 */
 
 resource "aws_cognito_identity_pool" "main" {
+  provider                         = "aws.west"
   identity_pool_name               = "OSHA ML Predict"
   allow_unauthenticated_identities = true
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "redshift_password" {
 output "redshift_user" {
   value = "mcp"
 }
+
+output "cognito_identity_pool" {
+  value = "${aws_cognito_identity_pool.main}"
+}
