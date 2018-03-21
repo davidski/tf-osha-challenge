5314a3bf35d3b2c7d7928842b1068863e86fd2a1
David F. Severski <davidski@deadheaven.com>
2018-03-21 11:52:17 -0700

    Add policy attachments to Cognito pool

--- cognito.tf
+++ cognito.tf
@@ -9,3 +9,12 @@ resource "aws_cognito_identity_pool" "main" {
   identity_pool_name               = "OSHA ML Predict"
   allow_unauthenticated_identities = true
 }
+
+resource "aws_cognito_identity_pool_roles_attachment" "main" {
+  identity_pool_id = "${aws_cognito_identity_pool.main.id}"
+
+  roles {
+    "authenticated"   = "arn:aws:iam::754135023419:role/Cognito_OSHAMLPredictAuth_Role"
+    "unauthenticated" = "arn:aws:iam::754135023419:role/Cognito_OSHAMLPredictUnauth_Role"
+  }
+}
