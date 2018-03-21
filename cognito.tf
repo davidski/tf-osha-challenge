f32043e48684e9a0b4e2bac6a21062ac37241b57
David F. Severski <davidski@deadheaven.com>
2018-03-21 15:43:13 -0700

    Unauth Cognito role still needs Cognito permissions in addition to ML perms

--- cognito.tf
+++ cognito.tf
@@ -26,6 +26,11 @@ resource "aws_iam_role" "unauthenticated" {
   assume_role_policy = "${data.aws_iam_policy_document.unauthenticated_trust.json}"
 }
 
+resource "aws_iam_role_policy_attachment" "oshaml_cognito" {
+  role       = "${aws_iam_role.unauthenticated.name}"
+  policy_arn = "${aws_iam_policy.unauthenticated_policy.arn}"
+}
+
 resource "aws_iam_role_policy_attachment" "oshaml_ml_readonly" {
   role       = "${aws_iam_role.unauthenticated.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonMachineLearningReadOnlyAccess"
